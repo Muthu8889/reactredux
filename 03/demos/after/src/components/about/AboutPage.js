29472a6c1b62d0de8af0903be6c6e5bcee85acc1
Ubuntu <ubuntu@ip-172-31-36-97.ec2.internal>
2019-07-16 11:44:18 +0000

    -m "Adding new thingd
    "

---
+++ 03/demos/after/src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div className="jumbotron">
+    <h2>About Page</h2>
+    <p> this uses react, redux and react router and many helpful library.</p>
+  </div>
+);
+
+export default AboutPage;
--- 03/demos/before/webpack.config.dev.js
+++ 03/demos/before/webpack.config.dev.js
@@ -7,17 +7,17 @@ process.env.NODE_ENV = "development";
 module.exports = {
   mode: "development",
   target: "web",
-  devTool: "cheap-module-source-map",
+  devtool: "cheap-module-source-map",
   entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
-    fileName: "bundle.js"
+    filename: "bundle.js"
   },
   devServer: {
     stats: "minimal",
     overlay: true,
-    histroyApiFallback: true,
+    historyApiFallback: true,
     disableHostCheck: true,
     headers: { "Access-Control-Allow-Origin": "*" },
     https: false
